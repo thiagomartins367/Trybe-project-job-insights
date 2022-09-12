f282f725a715b5ce8d61b861924d9cddd736d397
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-09-12 20:38:41 -0300

    feat(tests/counter/test_counter.py): Função de teste "test_counter()", para testar função "count_ocurrences", criada e implementada - Requisito 10

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,20 @@
-# from src.counter import count_ocurrences
+import pytest
+from src.counter import count_ocurrences
+
+PYTHON = "Python"
+JAVASCRIPT = "JaVaScRiPt"
+FILE_PATH = "src/jobs.csv"
 
 
 def test_counter():
-    pass
+    assert count_ocurrences(FILE_PATH, PYTHON) == 1639
+    assert count_ocurrences(FILE_PATH, JAVASCRIPT) == 122
+    with pytest.raises(
+        AttributeError, match="'bool' object has no attribute 'lower'"
+    ):
+        count_ocurrences(FILE_PATH, True)
+    with pytest.raises(
+        FileNotFoundError,
+        match="No such file or directory: 'src/jobs.cs'",
+    ):
+        count_ocurrences("src/jobs.cs", PYTHON)
