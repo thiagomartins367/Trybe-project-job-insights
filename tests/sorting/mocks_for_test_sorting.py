9144db1720e98ec0460e7789abcfe6079bb7c473
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-09-13 16:59:46 -0300

    feat(tests/sorting/mocks_for_test_sorting.py): Mock para a função teste "test_sort_by_criteria()" criado - Requisito 12

---
+++ tests/sorting/mocks_for_test_sorting.py
@@ -0,0 +1,131 @@
+JOBS = [
+  {
+    "job_title": "Marketing",
+    "min_salary": "",
+    "max_salary": "",
+    "date_posted": "2020-05-08"
+  },
+  {
+    "job_title": "Registered Nurse",
+    "min_salary": "",
+    "max_salary": "",
+    "date_posted": "2020-04-25"
+  },
+  {
+    "job_title": "Senior Salesforce Developer",
+    "min_salary": "44587",
+    "max_salary": "82162",
+    "date_posted": "2020-05-08"
+  },
+  {
+    "job_title": "DEPUTY EXECUTIVE DIRECTOR, PROGRAM AND LEGAL ADVOCACY",
+    "min_salary": "125410",
+    "max_salary": "212901",
+    "date_posted": "2020-04-28"
+  },
+  {
+    "job_title": "Emergency Veterinarian - NYC",
+    "min_salary": "94715",
+    "max_salary": "103279",
+    "date_posted": "2020-05-05"
+  },
+]
+
+JOBS_MIN_SALARY = [
+  {
+    "job_title": "Senior Salesforce Developer",
+    "min_salary": "44587",
+    "max_salary": "82162",
+    "date_posted": "2020-05-08"
+  },
+  {
+    "job_title": "Emergency Veterinarian - NYC",
+    "min_salary": "94715",
+    "max_salary": "103279",
+    "date_posted": "2020-05-05"
+  },
+  {
+    "job_title": "DEPUTY EXECUTIVE DIRECTOR, PROGRAM AND LEGAL ADVOCACY",
+    "min_salary": "125410",
+    "max_salary": "212901",
+    "date_posted": "2020-04-28"
+  },
+  {
+    "job_title": "Marketing",
+    "min_salary": "",
+    "max_salary": "",
+    "date_posted": "2020-05-08"
+  },
+  {
+    "job_title": "Registered Nurse",
+    "min_salary": "",
+    "max_salary": "",
+    "date_posted": "2020-04-25"
+  }
+]
+
+JOBS_MAX_SALARY = [
+  {
+    "job_title": "DEPUTY EXECUTIVE DIRECTOR, PROGRAM AND LEGAL ADVOCACY",
+    "min_salary": "125410",
+    "max_salary": "212901",
+    "date_posted": "2020-04-28"
+  },
+  {
+    "job_title": "Emergency Veterinarian - NYC",
+    "min_salary": "94715",
+    "max_salary": "103279",
+    "date_posted": "2020-05-05"
+  },
+  {
+    "job_title": "Senior Salesforce Developer",
+    "min_salary": "44587",
+    "max_salary": "82162",
+    "date_posted": "2020-05-08"
+  },
+  {
+    "job_title": "Marketing",
+    "min_salary": "",
+    "max_salary": "",
+    "date_posted": "2020-05-08"
+  },
+  {
+    "job_title": "Registered Nurse",
+    "min_salary": "",
+    "max_salary": "",
+    "date_posted": "2020-04-25"
+  }
+]
+
+JOBS_DATE_POSTED = [
+  {
+    "job_title": "Senior Salesforce Developer",
+    "min_salary": "44587",
+    "max_salary": "82162",
+    "date_posted": "2020-05-08"
+  },
+  {
+    "job_title": "Marketing",
+    "min_salary": "",
+    "max_salary": "",
+    "date_posted": "2020-05-08"
+  },
+  {
+    "job_title": "Emergency Veterinarian - NYC",
+    "min_salary": "94715",
+    "max_salary": "103279",
+    "date_posted": "2020-05-05"
+  },
+  {
+    "job_title": "DEPUTY EXECUTIVE DIRECTOR, PROGRAM AND LEGAL ADVOCACY",
+    "min_salary": "125410",
+    "max_salary": "212901",
+    "date_posted": "2020-04-28"
+  },
+  {
+    "job_title": "Registered Nurse",
+    "min_salary": "",
+    "max_salary": "",
+    "date_posted": "2020-04-25"
+  }
+]
