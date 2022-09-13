bae705cfeacd1a3697e6cfbc29b6a771eff17f9e
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-09-13 16:12:01 -0300

    feat(tests/brazilian/test_brazilian_jobs.py): Função de teste "test_brazilian_jobs()", para testar função "read_brazilian_file", criada e implementada - Requisito 11

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,21 @@
-# from src.brazilian_jobs import read_brazilian_file
+import pytest
+from src.brazilian_jobs import read_brazilian_file
+
+BRAZILIAN_JOB = {
+  "title": "Ux Designer",
+  "salary": "3000",
+  "type": " full time"
+}
 
 
 def test_brazilian_jobs():
-    pass
+    brazilian_jobs = read_brazilian_file(
+        "tests/mocks/brazilians_jobs.csv"
+    )
+    for job in brazilian_jobs:
+        assert job.keys() == BRAZILIAN_JOB.keys()
+    with pytest.raises(
+        FileNotFoundError,
+        match="No such file or directory: 'tests/mocks/brazilians_jobs.cs'",
+    ):
+        read_brazilian_file("tests/mocks/brazilians_jobs.cs")
