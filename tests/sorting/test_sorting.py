1c23ee335e4ac585c417067255e0abd6615c4444
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-09-13 17:01:18 -0300

    feat(tests/sorting/test_sorting.py): Função de teste "test_sort_by_criteria()", para testar função "sort_by", criada e implementada - Requisito 12

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,18 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
+from tests.sorting.mocks_for_test_sorting import (
+    JOBS,
+    JOBS_DATE_POSTED,
+    JOBS_MAX_SALARY,
+    JOBS_MIN_SALARY,
+)
 
 
 def test_sort_by_criteria():
-    pass
+    sort_by(JOBS, "min_salary")
+    assert JOBS == JOBS_MIN_SALARY
+
+    sort_by(JOBS, "max_salary")
+    assert JOBS == JOBS_MAX_SALARY
+
+    sort_by(JOBS, "date_posted")
+    assert JOBS == JOBS_DATE_POSTED
